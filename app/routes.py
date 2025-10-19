f5c6ca69c068b527e25f41e921544cfc746dbb01
nedyah212 <hploszay2@academic.rrc.ca>
2025-10-19 12:18:18 -0500

    updated routes to reflect my current figma routing visual

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for
 from .controllers import Controller
+from .services import Services
+from .models import Shipment
 
 main = Blueprint("main", __name__)
 
@@ -9,4 +11,44 @@ def home():
 
 @main.route("/create_shipment", methods=["GET", "POST"])
 def create_shipment():
-  return render_template("create_shipment.html", form=Controller.create_shipment())
+
+  form = Controller.create_shipment()
+  if form.validate_on_submit():
+    success = Services.DatabaseMethods.create_shipment(form.data)
+
+    if success:
+      return redirect('/')
+
+  return render_template("create_shipment.html", form=form)
+
+@main.route("/show_shipment/<registration_number>")
+def show_shipment(registration_number):
+  return render_template('show_shipment.html', registration_number=registration_number)
+
+@main.route("/edit_type/<registration_number>")
+def show_edit_shipment_type(registration_number):
+  return render_template("edit_shipment_type.html", registration_number=registration_number)
+
+@main.route("/edit_type/<registration_number>/floor")
+def edit_type_floor(registration_number):
+  return render_template("edit_type_floor.html", registration_number=registration_number)
+
+@main.route("/edit_type/<registration_number>/pallet")
+def edit_type_pallet(registration_number):
+  return render_template("edit_type_pallet.html", registration_number=registration_number)
+
+@main.route("/edit_type/<registration_number>/trailer")
+def edit_type_trailer(registration_number):
+  return render_template("edit_type_trailer.html", registration_number=registration_number)
+
+@main.route("/settings")
+def settings():
+  return render_template("settings.html")
+
+@main.route("/settings/pallet_manager")
+def pallet_manager():
+  return render_template("pallet_manager.html")
+
+@main.route("/settings/trailer_manager")
+def trailer_manager():
+  return render_template("trailer_manager.html")
--- app/services.py
+++ app/services.py
@@ -1,6 +1,7 @@
 from . import db
 from .logging import logger
 from sqlalchemy.exc import IntegrityError, OperationalError, SQLAlchemyError
+from .models import Shipment
 
 class Services():
 
@@ -15,18 +16,22 @@ class Services():
 
   class DatabaseMethods():
 
-    def create_shipment(shipment):
+    def create_shipment(form_data):
+      shipment = Shipment(**form_data)
       try:
           db.session.add(shipment)
           db.session.commit()
           logger.info(f"Success: {shipment} has been added to the database.")
-
+          return True
       except IntegrityError as e:
           logger.error(f"IntegrityError: {e}")
           db.session.rollback()
+          return False
       except OperationalError as e:
           logger.error(f"OperationalError: {e}")
           db.session.rollback()
+          return False
       except SQLAlchemyError as e:
           logger.error(f"SQLAlchemyError: {e}")
           db.session.rollback()
+          return False
