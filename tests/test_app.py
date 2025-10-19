90145e782cf7b784a4bd0cb5705fb928eac78bd1
nedyah212 <hploszay2@academic.rrc.ca>
2025-10-18 23:18:47 -0500

    solve .env import problem in test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,11 +5,12 @@ import sys
 from app.forms import Forms
 from app.services import Services
 from wtforms import ValidationError
+from dotenv import load_dotenv
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import create_app, db
-
+load_dotenv()
 
 @pytest.fixture
 def app():
